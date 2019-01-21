c02066abe5e218567bcdc0e3860e46dac96d0416
armen.sadoyan <arm.sadoyan@gmail.com>
2019-01-21 12:20:46 +0400

    Updates

---
+++ precache-manifest.348b5c1f271773f54949b2b16518f64f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f2b05feffa477a31256",
+    "url": "/formik-duck/static/css/main.af17f16d.chunk.css"
+  },
+  {
+    "revision": "6f2b05feffa477a31256",
+    "url": "/formik-duck/static/js/main.6f2b05fe.chunk.js"
+  },
+  {
+    "revision": "50704b3c53fc28efe933",
+    "url": "/formik-duck/static/js/1.50704b3c.chunk.js"
+  },
+  {
+    "revision": "4104397f234f8ea155cd",
+    "url": "/formik-duck/static/js/runtime~main.4104397f.js"
+  },
+  {
+    "revision": "d375c4600329ad800762e3a360543520",
+    "url": "/formik-duck/index.html"
+  }
+];
